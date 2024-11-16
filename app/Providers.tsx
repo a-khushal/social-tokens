eb34b5219d318ddacfa075bd9a2a433d8190ba49
NIDHI SHARMA <114356358+Nidhi-Sharma9419@users.noreply.github.com>
2024-11-16 13:25:31 +0530

    Update Providers.tsx

--- app/Providers.tsx
+++ app/Providers.tsx
@@ -3,17 +3,19 @@
 import { SessionProvider } from "next-auth/react";
 import { WalletProvider, ConnectionProvider } from "@solana/wallet-adapter-react";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
+import { useMemo } from "react";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
    
-    //const endpoint = useMemo(() => "https://api.devnet.solana.com", []);
+    const endpoint = useMemo(() => "https://api.devnet.solana.com", []);
 
-    //const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
+    const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
 
     return (
         <SessionProvider>
-            <ConnectionProvider endpoint={process.env.RPC_URL || "https://api.devnet.solana.com"}>
-                    <WalletProvider wallets={[]}>
+            <ConnectionProvider endpoint={endpoint}>
+                    <WalletProvider wallets={wallets}>
                         <WalletModalProvider>
                             {children}
                         </WalletModalProvider>
